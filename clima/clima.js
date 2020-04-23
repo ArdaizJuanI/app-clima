936ffa01590f67ffef0028ab4d8e529c765720cf
ArdaizJuanI <48576195+ArdaizJuanI@users.noreply.github.com>
2020-04-23 08:53:55 -0300

    Update clima.js

--- clima/clima.js
+++ clima/clima.js
@@ -5,8 +5,15 @@ const getClima = async (lat,lng) => {
 
     const resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=4f9225ff97100cf3f2a7714fa9fab541&units=metric`)
 
+    const temp=resp.data.main.temp;
+    const viento=resp.data.wind.speed;
+    const humedad=resp.data.main.humidity;
+    
     //destructuracion
-    return resp.data.main.temp
+    return {temp,
+           viento,
+           humedad,
+           }
 
 }
 
